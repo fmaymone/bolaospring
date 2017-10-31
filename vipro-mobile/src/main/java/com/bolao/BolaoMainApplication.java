de4eef7eb42decb57cd9fa4721691d08a51fdc07
Fernando Maymone <fmaymone@@gmail.com>
2017-10-31 10:46:26 -0300

    changing

--- vipro-mobile/src/main/java/com/bolao/BolaoMainApplication.java
+++ vipro-mobile/src/main/java/com/bolao/BolaoMainApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ViproMobileApplication {
+public class BolaoMainApplication {
 
 	public static void main(String[] args) {	
-		SpringApplication.run(ViproMobileApplication.class, args);
+		SpringApplication.run(BolaoMainApplication.class, args);
 	}
 }
