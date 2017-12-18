a92f3be17b20c87fb0744832f45c93d6747c6232
49787618 <49787618@E381731.sefaz-ce.gov.br>
2017-12-18 15:27:40 -0300

    Added API path end CrossOrigin to matches

--- vipro-mobile/src/main/java/com/bolao/repository/jpa/MatchRepositoryJpa.java
+++ vipro-mobile/src/main/java/com/bolao/repository/jpa/MatchRepositoryJpa.java
@@ -3,11 +3,12 @@ package com.bolao.repository.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-import com.bolao.entity.Bet;
 import com.bolao.entity.Match;
 
 @RepositoryRestResource(path="matches")
+@CrossOrigin
 public interface MatchRepositoryJpa extends JpaRepository<Match,Long> {
 	
 
